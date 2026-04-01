63e703f70fc27147fbcd079f671f614576fd84eb
qusikk <mamedovqusik561@gmail.com>
2026-04-01 13:48:48 +0400

    Create Homework_9_1.java
    dz 01.04.2026

---
+++ src/homework_9/Homework_9_1.java
@@ -0,0 +1,52 @@
+package homework_9;
+
+public class Homework_9_1 {
+	public static void main(String[] args) {
+	    byte b = 10;  //8 bit
+		short s = 300; //16 bit
+		int i = 1000; //32 bit
+		long l = 10000l; //64 bit
+		float f = 9.24f; //32 bit
+		double d = 9.2459; //64 bit
+		char c = 'A'; //16 bit
+		boolean rnlda = true; //1 bit
+		
+		//2 
+		
+		
+		long population = 100_000_000L;
+		System.out.println(population);
+//		bez l sistema dumaet cto eto int a podcerkivaniye dla udobstva
+		
+		
+//		3
+		float ff = 1.23f;
+		double dd = 1.23;
+		System.out.println("float" + "=" + ff);
+		System.out.println("double" + "=" + dd);
+//		bez f sistema dumaet cto float eto double
+		
+		
+		
+//		4
+		boolean ready = true;
+		char grade = 'B';
+		System.out.println(ready);
+		System.out.println(grade);
+		char nl = '\n';
+		System.out.print("End" + nl);
+		System.out.println("End");
+		
+//		5
+	
+		
+		int ii = 8;
+		System.out.println(ii); //esli mi ne dobavim vnutr int ctoto to on vivedet error
+		
+		
+		
+		
+		
+	}
+
+}
