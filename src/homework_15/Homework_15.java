b2730d318bb047c260a1d5686b14c812acc333b0
qusikk <mamedovqusik561@gmail.com>
2026-05-10 21:54:55 +0400

    classwork 10.05.2026

---
+++ src/homework_15/Homework_15.java
@@ -0,0 +1,29 @@
+package homework_15;
+
+public class Homework_15 {
+	public static void main(String[] args) {
+		 for (int i = 1; i <= 10; i++) {
+	            System.out.println(i + " ");}
+	            
+	            
+	            for (int i2 = 0; i2 <= 20; i2+=2) {
+	                System.out.print(i2 + " ");}    
+	            
+	            int sum = 0;
+	            
+	            for (int s = 1; s <= 100; s++) {
+	            	sum += s;}
+	            System.out.println("sum" + sum);
+	            
+	           
+	            for(int u = 1 ; u <= 10;u++ ) {
+	            	System.out.println("7 " + "x " + u + " = " + u * 7 );
+	            }
+	            
+	            for(int y = 10; y >= 0;y--) {
+	            	System.out.println(y);}
+					
+			
+	}
+
+	}
